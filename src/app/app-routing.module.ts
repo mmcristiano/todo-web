951fd1bc17d733ab66dd4e0cd7893fb1b3ed0ec1
mmcristiano <maiamoraiscristiano@gmail.com>
2021-01-24 18:46:33 -0300

    Login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,8 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path : '', 
-    component: HomeComponent
+    path: '',
+    component: LoginComponent
   }
 ];
 
