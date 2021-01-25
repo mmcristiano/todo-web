5b792c4651465a5ba5f3daf7dbba7b643a0e944a
mmcristiano <maiamoraiscristiano@gmail.com>
2021-01-24 21:30:23 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { NewComponent } from './pages/new/new.component';
+import { AllComponent } from './pages/all/all.component';
+import { TomorrowComponent } from './pages/tomorrow/tomorrow.component';
+import { TodayComponent } from './pages/today/today.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
@@ -5,14 +9,27 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
+
   {
     path: 'login',
     component: LoginComponent,
-  }
+  },
+  {
+    path: '', component: HomeComponent, children: [
+      {
+        path: '', component: TodayComponent
+      },
+      {
+        path: 'tomorrow', component: TomorrowComponent
+      },
+      {
+        path: 'all', component: AllComponent
+      },
+      {
+        path: 'new', component: NewComponent
+      },
+    ],
+  },
 ];
 
 @NgModule({
